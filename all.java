4c82143fbf4c1e4b3193e8335e3811832584ad8e
Your Name <you@example.com>
2026-06-17 14:16:22 +0500

    first commit

---
+++ all.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner input = new Scanner(System.in);
+	System.out.println("Please input a , b , c ");
+	int a = input.nextInt();
+	int b = input.nextInt();
+	int c = input.nextInt();
+	
+	int max = a;
+	if(b > max) max = b;
+	if(c > max) max = c;
+	System.out.println("max =" +max);
+	}
+}
